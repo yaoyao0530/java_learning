997ae53f9e9c213725a11f6bffe178a4c4ac37be
yaoyao0530 <946919605@qq.com>
2014-06-26 22:59:41 +0800

    加入文档

---
+++ AddNumber.java
@@ -0,0 +1,14 @@
+public class AddNumber{
+	public static void main(String args[]){
+		int n = 3;
+		int m = Integer.MAX_VALUE;
+		float j = 3.56f;
+		boolean x = true;
+		System.out.print("3 * 3 = " + n * n);
+		System.out.print(" "+ m);
+		String str = "LoveYH";
+		System.out.println(str + n);
+		System.out.println((int)j);
+		System.out.println(!x);
+	}
+}
